ec10f6041b5a48e8cee8fbb4c5cc5f848c83a55a
Aaron Pittenger <pittenger.aaron@gmail.com>
2018-06-09 11:26:49 -0400

    Updated to also read in the test points. First cut at finding what areas the points are in

--- neighborhood-decoder.py
+++ neighborhood-decoder.py
@@ -1,6 +1,7 @@
 import os
 
 class Coordinate:
+    name = ""
     latitude = 0
     longitude = 0
 
@@ -8,14 +9,19 @@ class Coordinate:
         self.latitude = lat
         self.longitude = lon
 
+    def __init__(self, name, lat, lon):
+        self.latitude = lat
+        self.longitude = lon
+        self.name = name
+
     def __str__(self):
         return "(%.18f, %.18f)" % (self.latitude, self.longitude)
 
 class Neighborhood:
     name = ""
     points = []
-    maxPoint = Coordinate(-90, -180)
-    minPoint = Coordinate(90, 180)
+    maxPoint = Coordinate("", -90, -180)
+    minPoint = Coordinate("", 90, 180)
 
     def __str__(self):
         return self.name + ": " + str(self.maxPoint) + " " + str(self.minPoint)
@@ -27,16 +33,16 @@ def readNeighborhoods():
         for line in f:
             if line.strip().startswith("-"):
                 lat, lon = line.strip(' \n').split(',')
-                newPoint = Coordinate(float(lat), float(lon))
+                newPoint = Coordinate("", float(lat), float(lon))
                 newHood.points.append(newPoint)
-                if(newPoint.latitude > newHood.maxPoint.latitude):
-                    newHood.maxPoint.latitude = newPoint.latitude
-                if(newPoint.longitude > newHood.maxPoint.longitude):
-                    newHood.maxPoint.longitude = newPoint.longitude
-                if(newPoint.latitude < newHood.minPoint.latitude):
-                    newHood.minPoint.latitude = newPoint.latitude
-                if(newPoint.longitude < newHood.minPoint.longitude):
-                    newHood.minPoint.longitude = newPoint.longitude
+                if(newPoint.latitude > newHood.maxPoint.latitude and newPoint.longitude > newHood.maxPoint.longitude):
+                    newHood.maxPoint = newPoint
+
+                if(newPoint.latitude < newHood.minPoint.latitude and newPoint.longitude < newHood.minPoint.longitude):
+                    newHood.minPoint = newPoint
+
+            elif line.strip() == "":
+                continue
             else:
                 try:
                     hoodList.append(newHood)
@@ -44,10 +50,23 @@ def readNeighborhoods():
                     print "Just the start - creating a new hood!"
 
                 newHood = Neighborhood()
-                newHood.name = line.strip(' :\n')
-
+                newHood.name = line.strip(' \n').rstrip(':')
 
     return hoodList
 
+def readTestPoints():
+    testPoints = []
+    with open('test-points.txt') as f:
+        for line in f:
+            name, datapoint = line.split(':')
+            lat, lon = datapoint.split(',')
+            testPoints.append(Coordinate(name, float(lat), float(lon)))
+
+    return testPoints
+
 hoodList = readNeighborhoods()
-    
+testList = readTestPoints()
+for point in testList:
+    for hood in hoodList:
+        if point.latitude > hood.minPoint.latitude and point.latitude < hood.maxPoint.latitude and point.longitude > hood.minPoint.longitude and point.longitude < hood.maxPoint.longitude:
+            print point.name + " is in hood " + hood.name
